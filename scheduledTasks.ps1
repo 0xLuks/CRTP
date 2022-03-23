284d49caf5610551fba7470f069b8b2cc3d9c071
Luks <51793738+0xLuks@users.noreply.github.com>
2022-03-23 14:59:44 +0100

    Update scheduledTasks.ps1

--- scheduledTasks.ps1
+++ scheduledTasks.ps1
@@ -1,11 +1,10 @@
-Utile pour DonPAPI
-----------------------------------------------------------------------------------
+# Utile pour DonPAPI (extraction des secrets comme les utilisateurs/mot de passe des utilisateurs qui exécutent des tâches planifiées)
 
-### Lister le nombre de tâches planifiées
+# Lister le nombre de tâches planifiées
 (Get-ScheduledTask).Count
 
-### Lister les tâches planifiées (path, taskName, state)
+# Lister les tâches planifiées (path, taskName, state)
 Get-ScheduledTask
 
-### Obtenir plus d'infos sur une tâche spécifique (UserId, RunLevel, Id...)
+# Obtenir plus d'infos sur une tâche spécifique (UserId, RunLevel, Id...)
 Get-ScheduledTask {TASKNAME} | select -ExpandProperty Principal
